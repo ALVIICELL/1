8f9439f3f3f3ea4b58c767ea1e051df2225d6b4f
ALVIICELL <137196208+ALVIICELL@users.noreply.github.com>
2023-06-27 12:31:43 +0700

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,3 +1,57 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###########- COLOR CODE -##############
+echo -e " [INFO] Downloading Update File"
+sleep 2
+# hapus menu
+rm -rf menu
+rm -rf menu-trial
+rm -rf menu-vmess
+rm -rf menu-vless
+rm -rf add-vless
+rm -rf running
+rm -rf clearcache
+rm -rf menu-trgo
+rm -rf menu-trojan
+rm -rf cf
+rm -rf menu-ssh
+rm -rf usernew
+rm -rf trial
+rm -rf renew
+rm -rf hapus
+rm -rf cek
+rm -rf member
+rm -rf delete
+rm -rf autokill
+rm -rf ceklim
+rm -rf tendang
+rm -rf menu-set
+rm -rf menu-domain
+rm -rf add-host
+rm -rf port-change
+rm -rf certv2ray
+rm -rf menu-webmin
+rm -rf speedtest
+rm -rf about
+rm -rf auto-reboot
+rm -rf restart
+rm -rf bw
+rm -rf port-ssl
+rm -rf port-ovpn
+rm -rf xp
+rm -rf acs-set
+rm -rf sshws
+rm -rf status
+rm -rf menu-backup
+rm -rf backup
+rm -rf restore
+rm -rf instal-udp
+rm -rf jam
+rm -rf up
+rm -rf genssl
+rm -rf slow
+
 # download script
 cd /usr/bin
 # menu
@@ -106,3 +160,6 @@ chmod +x menu-backup
 chmod +x backup
 chmod +x restore
 chmod +x jam
+echo -e " [INFO] Update Successfully"
+sleep 2
+menu
